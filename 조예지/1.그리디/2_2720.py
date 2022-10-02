3eef2fb8dfdce5d5f6f1905bbc1f17ce8313bc36
Yeji <whdpwl2@gmail.com>
2022-10-02 12:20:36 +0900

    [BOJ] 2720 세탁소 사장 동혁

--- 조예지/1.그리디/2_2720.py
+++ 조예지/1.그리디/2_2720.py
@@ -1,12 +1,8 @@
 T = int(input())
-
+coins = [25, 10, 5, 1]
 for _ in range (T):
   chg = int(input())
-
-  Q = chg // 25
-  chg %= 25
-  D = chg // 10
-  chg %= 10
-  N = chg // 5
-  P = chg % 5
-  print (Q, D, N, P)
+  for c in coins :
+    n = chg // c
+    chg %= c
+    print(n, end=" ")
