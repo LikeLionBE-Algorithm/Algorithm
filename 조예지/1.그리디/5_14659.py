c7ff4821672dc5f9e6a9281cf927611afcd6f0d0
JO YEJI <whdpwl2@gmail.com>
2022-10-05 00:21:19 +0900

    [BOJ] 14659 한조서열정리하고옴ㅋㅋ

--- 조예지/1.그리디/5_14659.py
+++ 조예지/1.그리디/5_14659.py
@@ -1,19 +1,23 @@
 N = int(input())
 
 arr = list(map(int, input().split()))
-result = 0
 
-for i in range(len(arr)-1) :
-    cnt = 0
-    for j in range(i+1, len(arr)) :
-        if arr[i]>arr[j]:
-            cnt += 1
-        else:
-            break
-    if cnt > result :
-        result = cnt
-        # 뒤에 볼 필요가 없으면 탈출
-        if (i+result*2) >= len(arr) :
-            break
+H = 0
+result = 0
+cnt = 0
+for m in arr:
+    #낮은 봉우리는 처치
+    if m < H :
+        cnt += 1
+    #높은 봉우리는 초기화 & 처치한 수 비교해서 더 많으면 저장하기
+    else :
+        H = m
+        if cnt > result:
+            result = cnt
+        cnt = 0
+
+#마지막 봉우리까지 비교해주기
+if cnt > result:
+    result = cnt
 
 print(result)
