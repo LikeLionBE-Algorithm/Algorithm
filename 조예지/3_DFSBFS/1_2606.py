abb4a602623d5f833c5d54eefdf2f290fdc3cd92
JO YEJI <whdpwl2@gmail.com>
2022-10-16 16:39:34 +0900

    [BOJ] 2606 바이러스

--- 조예지/3_DFSBFS/1_2606.py
+++ 조예지/3_DFSBFS/1_2606.py
@@ -1,9 +1,15 @@
 N = int(input())
 M = int(input())
 
-# graph 만들기
-# graph = [[]]*(N+1)
+def dfs (graph, infected, i):
+    infected[i] = True
+    for com in graph[i]:
+        if not infected[com] :
+            dfs(graph, infected, com)
 
+
+# graph 만들기
+# graph = [[]]*(N+1) : 이렇게 했다가 graph 안의 값들이 동기화 됨
 graph = [[] for _ in range(N+1)]
 
 for _ in range(M):
@@ -12,12 +18,6 @@ for _ in range(M):
     # 아래 코드 없어서 반대로 상황은 체크가 안 됨
     graph[b].append(a)
 
-
-def dfs (graph, infected, i):
-    infected[i] = True
-    for com in graph[i]:
-        if not infected[com] :
-            dfs(graph, infected, com)
 
 
 infected = [False]*(N+1)
