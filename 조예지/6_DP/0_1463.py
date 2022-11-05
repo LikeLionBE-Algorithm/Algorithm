a9c50096b806b89b8fc8f79746ff2150c685ea2c
JO YEJI <whdpwl2@gmail.com>
2022-11-05 22:30:42 +0900

    [BOJ] 1463 1로 만들기 - 통과

--- 조예지/6_DP/0_1463.py
+++ 조예지/6_DP/0_1463.py
@@ -3,11 +3,12 @@
 함수
 결과는 1
 연산을 사용하는 횟수의 최소값
+통과x
 """
 
 num = int(input())
 
-cnt = [0]*(num+1)
+cnt = [0]*(num+3)
 cnt[1] = 0
 cnt[2] = 1
 cnt[3] = 1
@@ -15,13 +16,12 @@ cnt[3] = 1
 
 for N in range(4, num+1):
     if N%3 == 0:
-        cnt[N] = cnt[N//3]+1
-    elif N%3 == 1:
-        cnt[N] = cnt[N-1]+1
-    else:
         if N%2 == 0:
-            cnt[N] = cnt[N//2]+1
+            cnt[N] = min(cnt[N//2]+1, cnt[N//3]+1, cnt[N-1]+1)
         else:
-            cnt[N] = cnt[N-1]+2
-
+            cnt[N] = min(cnt[N//3]+1, cnt[N-1]+1)
+    elif N%2 == 0:
+        cnt[N] = min(cnt[N//2]+1, cnt[N-1]+1)
+    else:
+        cnt[N] = cnt[N-1]+1
 print(cnt[num])
